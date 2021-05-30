220c0901f1fcc6e10262daded2c2f625257ffe49
kirilla111 <kirill_afanasyev00@mail.ru>
2021-05-30 20:28:18 +0300

    project

---
+++ ЛР13/vue_project/src/components/card.vue
@@ -0,0 +1,116 @@
+<template>
+    <div>
+        <div class="card__title">
+            <h3>{{title}}</h3>
+        </div>
+        <div class="card__text">
+            <div class="text__text">
+                <p>{{description}}</p>
+            </div>
+            <div class="form_item__title_text">
+                <p>{{date_start_text}}</p>
+            </div>
+            <div class="text__text">
+                <p>{{date_start}}</p>
+            </div>
+            <div class="form_item__title_text">
+                <p>{{owner_text}}</p>
+            </div>
+            <div class="text__text">
+                <p>{{owner}}</p>
+            </div>
+        </div>
+        <div class="card__options">
+            <img src="..\assets\btn_done.png" width="30" alt="edit">
+            <img src="..\assets\btn_edit.png" width="30" alt="done">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    date_start: {
+      type: Date,
+      default: ''
+    },
+    date_end: {
+      type: String,
+      default: ''
+    },
+    owner: {
+      type: String,
+      default: ''
+    },
+    card_index: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      date_start_text: 'Дата и время начала',
+      date_end_text: 'Ушло времяни',
+      owner_text: 'Ответственный',
+      highlightedStyles: {
+        color: 'white'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+    .lightMode_c{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        border-radius: 10px;
+        padding: 6%;
+        padding-top: 0;
+        background-color: #f1f9ff;
+        color: #219afb;
+        min-height: 200px;
+    }
+    .darkMode_c{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        border-radius: 10px;
+        padding: 6%;
+        padding-top: 0;
+        background-color: black;
+        color: white;
+        min-height: 200px;
+        border: 1px solid white;
+    }
+    .card__text{
+        padding-top: 0;
+        min-height: 150px;
+    }
+    .text__text{
+        color: inherit;
+        font-size: 0.8rem;
+    }
+    .card__options{
+        display: flex;
+        flex-direction: row;
+        justify-content:flex-end;
+        align-items:flex-end;
+        gap: 10px;
+    }
+    .form_item__title_text{
+        color: inherit;
+        font-size: 0.8rem;
+        font-weight: bold;
+    }
+</style>
---
+++ ЛР13/vue_project/src/components/tasksContainer.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="tasks_container">
+    <div class="task_container__title">
+      <h2>{{title}}</h2>
+    </div>
+    <div v-for="(card,index) in cards" :key="index"
+    >
+      <card v-bind:class="[{darkMode_c: toChange}, {lightMode_c: !toChange}]"
+      :title='card.title'
+      :date_start='card.date_start'
+      :owner='card.owner'
+      :card_index='index'
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import card from './card'
+
+export default {
+  name: 'tasksContainer',
+  components: { card },
+  props: {
+    title: String,
+    index: Number,
+    toChange: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      cards: [
+        {
+          title: 'title',
+          date_start: '1580558031264',
+          owner: 'owner'
+        },
+        {
+          title: 'title',
+          date_start: '1580558031264',
+          owner: 'title'
+        },
+        {
+          title: 'title',
+          date_start: '1580558031264',
+          owner: 'title'
+        }
+      ],
+      isSwitched: false
+    }
+  },
+  methods: {
+    switch_theme () {
+      this.isSwitched = !this.isSwitched
+    }
+  }
+}
+</script>
+
+<style>
+.tasks_container {
+  border: 1px solid #219afb;
+  background-color: inherit;
+  border-radius: 10px;
+  min-height: 500px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 0px;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+.tasks_container h2{
+  margin-top: 15px;
+  margin-bottom: 0px;
+}
+</style>
