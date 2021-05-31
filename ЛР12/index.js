52b9d59fcd28fec3d02507accc30ca96a11a9259
kirilla111 <kirill_afanasyev00@mail.ru>
2021-05-31 12:19:40 +0300

    lr 12 rewokred

--- ЛР12/index.js
+++ ЛР12/index.js
@@ -1,99 +1,15 @@
 var isSwitched = false;
 function switch_rotate(element) {
-    if (isSwitched) {
+    if (isSwitched){
         element.style.animation = "spin2 0.3s 1";
         element.style.animationFillMode = "forwards";
-
-        document.body.style.backgroundColor = "white";
-        var elems = document.getElementsByTagName("p");
-        elems[0].style.color = "black";
-        elems[1].style.color = "black";
-        for (i = 2; i < elems.length; i++) {
-            elems[i].style.color = "#219afb";
-        }
-        var elems = document.getElementsByTagName("h3");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "#219afb";
-        }
-        var elems = document.getElementsByTagName("h2");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "black";
-        }
-        var elems = document.getElementsByTagName("input");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].classList.remove('form__submit_black');
-            elems[i].style.backgroundColor = "white";
-        }
-        var elems = document.getElementsByClassName("task_container__card");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "#f1f9ff";
-            elems[i].style.border = "0px";
-        }
-        document.getElementById("form_id").style.backgroundColor = "white"
-        document.getElementById("form_id").style.border = "1px solid black"
-        document.getElementById("switch_text_1").style.color = "white"
-        document.getElementById("switch_text_2").style.color = "white"
-
-        var elems = document.getElementsByClassName("form_item__container");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "#f1f9ff";
-        }
-
-        var elems = document.getElementsByClassName("dropdown-content");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "#f1f9ff";
-            elems[i].style.color = "#219afb";
-        }
-        var elems = document.getElementsByTagName("input");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "#219afb";
-        }
-
-    } else {
+        document.getElementById('stylesheet').href='css/style.css';
+    }
+    else{
         element.style.animation = "spin1 0.3s 1";
         element.style.animationFillMode = "forwards";
-
-        document.body.style.backgroundColor = "black";
-        var elems = document.getElementsByTagName("p");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "#fff";
-        }
-        var elems = document.getElementsByTagName("h3");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "#fff";
-        }
-        var elems = document.getElementsByTagName("h2");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "#fff";
-        }
-        var elems = document.getElementsByTagName("input");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].classList.add('form__submit_black');
-            elems[i].style.backgroundColor = "black";
-        }
-        var elems = document.getElementsByClassName("task_container__card");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "black";
-            elems[i].style.border = "1px solid white";
-        }
-        document.getElementById("form_id").style.backgroundColor = "black"
-        document.getElementById("form_id").style.border = "1px solid white"
-
-        var elems = document.getElementsByClassName("form_item__container");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "black";
-
-        }
-        var elems = document.getElementsByClassName("dropdown-content");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.backgroundColor = "black";
-            elems[i].style.color = "white";
-        }
-        var elems = document.getElementsByTagName("input");
-        for (i = 0; i < elems.length; i++) {
-            elems[i].style.color = "white";
-        }
-    }
+        document.getElementById('stylesheet').href='css/dark-theme.css';
+    }    
     isSwitched = !isSwitched;
 }
 function add_card_form() {
